e81719f5e75cd3a7ee309a80edf80999e81355d6
Sultonkulov99 <abdukhoshim99@gmail.com>
2025-07-13 15:25:25 +0500

    auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './core/prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { RedisModule } from './common/redis/redis.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule, UsersModule, RedisModule],
   controllers: [],
   providers: [],
 })
